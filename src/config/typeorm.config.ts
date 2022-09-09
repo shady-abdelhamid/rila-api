6c4c11a10c42f0ab83bc38fae4d5a7166da8673d
Shady Abdelhamid <shady.abdelhamid@capiter.net>
2022-09-09 23:15:46 +0200

    chore: add mpngodb atlas endpoint

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,7 +4,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mongodb',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
-  url: process.env.MONGODB_URI || 'mongodb://localhost/rila-shady',
+  url: 'mongodb+srv://rila-user:jlgfM3lpsOsoeqSx@atlascluster.vkspryq.mongodb.net/?retryWrites=true&w=majority',
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
